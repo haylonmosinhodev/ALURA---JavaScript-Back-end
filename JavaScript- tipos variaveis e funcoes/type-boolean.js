a964c9282df0ba783ea253e28c0259b52d9337fb
Haylon <haylonmosinho.dev@gmail.com>
2023-01-28 20:22:07 -0300

    rota estudo

---
+++ JavaScript- tipos variaveis e funcoes/type-boolean.js
@@ -0,0 +1,15 @@
+// --------------------------------------
+// BOOLEAN
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// === usado para comparar dois valores
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Nanda";
+const texto2 = "N";
+
+console.log(texto1 === texto2);
+
+;
