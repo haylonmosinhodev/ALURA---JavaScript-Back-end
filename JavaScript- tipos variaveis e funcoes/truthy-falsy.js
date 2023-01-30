b65eced80206255ef721912c800710b38dc16fac
Haylon <haylonmosinho.dev@gmail.com>
2023-01-30 13:29:24 -0300

    codigos var e conversoes

---
+++ JavaScript- tipos variaveis e funcoes/truthy-falsy.js
@@ -0,0 +1,15 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// trythy or falsy
+
+// 0 ou " "= false
+// 1 = true
+
+console.log (0 == false) //true
+console.log (1 == true)
+
+// null
+// undefined - variavel sem valor;
--- JavaScript- tipos variaveis e funcoes/var-let-const.js
+++ JavaScript- tipos variaveis e funcoes/var-let-const.js
@@ -1,1 +1,36 @@
 // iniciar conteudo
+
+/*var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento
+
+var area; 
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log (area); */
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 10;
+let area;
+
+if (forma === 'quadrado'){
+    area = altura * comprimento;
+} else {area = (altura * comprimento) /2;
+}
+
+
+
+console.log(area)
